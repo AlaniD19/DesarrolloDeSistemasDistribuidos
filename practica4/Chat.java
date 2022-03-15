02fb55eb0211d48341c02782a75381e101109d86
Diana <dianapaodcs@gmail.com>
2022-03-15 08:36:41 -0600

    Avance de la práctica 4. (Recordar que es para versiones de java menores)

---
+++ practica4/Chat.java
@@ -0,0 +1,68 @@
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.MulticastSocket;
+import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class Chat {
+    
+    static void envia_mensajes_multicast(byte[] buffer, String ip, int puerto) throws IOException{
+        DatagramSocket socket = new DatagramSocket();
+        socket.send(new DatagramPacket(buffer, buffer.length, InetAddress.getByName(ip), puerto));
+        socket.close();
+    }
+    
+    static byte[] recibe_mensaje_multicast(MulticastSocket socket, int longitud_mensaje) throws IOException{
+        byte[] buffer = new byte[longitud_mensaje];
+        DatagramPacket paquete = new DatagramPacket(buffer, buffer.length);
+        socket.receive(paquete);
+        
+        return paquete.getData();
+    }
+    
+    static class Worker extends Thread{
+        public void run(){
+            try {
+                //en un ciclo infinito se recibiran los mensajes enviados al
+                //grupo 230.0.0.0 a través del puerto 50000 y se desplegarán en la pantalla
+                System.setProperty("java.net.preferIPv4Stack", "true");
+                
+                InetAddress grupo = InetAddress.getByName("230.0.0.0");
+                
+                MulticastSocket socket = new MulticastSocket(50000);
+                
+                socket.joinGroup(grupo);
+                
+                for(;;){
+                    byte[] mensaje_en_bytes = recibe_mensaje_multicast(socket, 1000);
+                    String mensaje = new String(mensaje_en_bytes, "UTF-8");
+                    System.out.println(mensaje);
+                }
+            } catch (IOException ex) {
+                Logger.getLogger(Chat.class.getName()).log(Level.SEVERE, null, ex);
+            }
+                        
+        }
+    }
+    
+    public static void main(String[] args) throws Exception{
+        new Worker().start();
+        String nombre_usuario = "Ana";
+        //String nombre_usuario = args[0];
+        //En un ciclo infinito se leera cada mensaje del teclado y se enviará el mensaje al 
+        //grupo 230.0.0.0 a través del puerto 50000
+        System.out.println("Usuario: " + nombre_usuario);
+        for(;;){
+            System.out.println("Ingrese el mensaje a enviar: ");
+            Scanner scanner = new Scanner(System.in);
+            String mensaje = scanner.nextLine();
+            
+            System.setProperty("java.net.preferIPv4Stack", "true");
+            mensaje = nombre_usuario + " dice " + mensaje;
+            envia_mensajes_multicast(mensaje.getBytes(), "230.0.0.0", 50000);
+        }
+    }
+}
